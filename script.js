696c99b2ccff38980d148a9e2cd3d3645a59ccb5
joy7pirate <160191509+joy7pirate@users.noreply.github.com.>
2024-11-14 23:12:25 +0000

    pour le choix de la machine j'ai d'abord fair une fonction qui contient un tableau ensit pour le choix j'ai utilise un Math.floor pour arrondir en decimale la plus proche du resultat de math.random ensuit j'ai multiplier la longuer de du tableau :)

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log("hello world")
 
 function getcomputerchoice(){
-    let choixmachine = ["pierre","papier","ciseaux"]
-    Math
+    const choixmachine = ["pierre","papier","ciseaux"]
+    // Math.randdom genere des nbre decimale entre [0,1[
+    // Math.floor arrondi au le nombre a virgule de Math.random() a l'entier < le plus proche
+    // choixmachine.length nous donne la taille du tableau 
+    const randomindex = Math.floor(Math.random()*choixmachine.length)
+    return choixmachine[randomindex];
 
 
 }
+console.log(getcomputerchoice());
+
+
+
+
