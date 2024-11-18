b0f75d0a4390a249f789dfc5f8c78216abb1d6b6
joy7pirate <160191509+joy7pirate@users.noreply.github.com.>
2024-11-18 19:52:25 +0000

    j'ai regler le probleme du choix huamin j'ai fait un tableau ensuite ajouter une condition et dans la condition et la condition j'ai mis choixhumain.include(popup)

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-console.log("hello world")
+let humainscore=0
+let machinescore=0
+
+// le choix de l'utilisateur
+function gethumainchoice(){
+    const choixhumain = ["pierre","papier","ciseaux"]
+
+let popup = prompt("donnez votre choix entre pierre, papier et ciseaux")
+if(choixhumain.includes(popup) ){
+ console.log("on commence")
+return popup
+ } else{
+       console.log("faite un choix entre pierre papier ciseaux")
+  }
+}
+console.log(gethumainchoice())
+
+
 
 function getcomputerchoice(){
     const choixmachine = ["pierre","papier","ciseaux"]
@@ -11,6 +28,7 @@ function getcomputerchoice(){
 
 }
 console.log(getcomputerchoice());
+// console.log(popup)
 
 
 
